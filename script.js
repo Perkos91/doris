28109f345e3053a6f52073313925666557c85085
Damian <damianstsiek@gmail.com>
2018-11-02 15:16:22 +0100

    add active class to menu with smooth scroll

--- script.js
+++ script.js
@@ -22,6 +22,52 @@ $("document").ready(function (e) {
     });
 });
 
+//.................Add active class to the current link in position.........//
+$(document).ready(function () {
+    $('a[href*=\\#]').bind('click', function (e) {
+        e.preventDefault(); // prevent hard jump, the default behavior
+
+        var target = $(this).attr("href"); // Set the target as variable
+        console.log(target);
+
+        // perform animated scrolling by getting top-position of target-element and set it as scroll target
+        $('html, body').stop().animate({
+            scrollTop: $(target).offset().top
+        }, 600, function () {
+            location.hash = target; //attach the hash (#jumptarget) to the pageurl
+        });
+
+        return false;
+    });
+});
+
+$(window).scroll(function () {
+    var scrollDistance = $(window).scrollTop();
+
+    // Show/hide menu on scroll
+    //if (scrollDistance >= 850) {
+    //		$('nav').fadeIn("fast");
+    //} else {
+    //		$('nav').fadeOut("fast");
+    //}
+
+    // Assign active class to nav links while scolling
+    $('section').each(function (i) {
+        if ($(this).position().top <= scrollDistance) {
+            $('.main-nav a.active-link').removeClass('active-link');
+            $('.main-nav a').eq(i).addClass('active-link');
+        }
+    });
+}).scroll();
+// $(window).scroll(function (event) {
+//     var scroll = $(window).scrollTop();
+//     $('.nav-links').toggleClass('active-link',
+//         //add 'ok' class when div position match or exceeds else remove the 'ok' class.
+//         scroll >= $('#jakpracujemy').offset().top
+//     );
+// });
+//trigger the scroll
+$(window).scroll(); //ensure if you're in current position when page is refreshed
 //...................Active class for information .........................//
 
 
