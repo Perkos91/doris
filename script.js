09985f176df7cc1c37f4bf3c5463af5448b63711
Damian <damianstsiek@gmail.com>
2018-11-05 20:52:34 +0100

    remove has from page url

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ $(document).ready(function () {
         $('html, body').stop().animate({
             scrollTop: $(target).offset().top
         }, 600, function () {
-            location.hash = target; //attach the hash (#jumptarget) to the pageurl
+            // location.hash = target; //attach the hash (#jumptarget) to the pageurl
         });
         return false;
     });
